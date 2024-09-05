427bd8de20ada87639170b96c41e0b4a35230d0c
Olav Fosse <post@olavfosse.no>
2024-09-05 23:21:37 +0200

    Dot plots :DD

--- nbs/the_littlest_learner.clj
+++ nbs/the_littlest_learner.clj
@@ -8,7 +8,7 @@
 
 (def line-xs [2.0 1.0 4.0 3.0])
 (def line-ys [1.8 1.2 4.2 3.3])
-#_(plot {:range [[-1 5] [-1 5]]
+(plot {:range [[-1 5] [-1 5]]
        ;; :cells-per-axis-unit 2
        :points (map vector line-xs line-ys)})
 
@@ -16,7 +16,12 @@
 
 
 (plot #(* 30 (Math/sin (/ % 10))))
-(plot identity)
+(plot {:range [[-1 3] [-1 3]]
+       :f #(Math/sin %)})
+
+(plot {:range [[-1 3] [-1 3]]
+       :f #(Math/sin %)})
+#_(plot identity)
 
 
 
--- src/no/olavfosse/malt.clj
+++ src/no/olavfosse/malt.clj
@@ -10,20 +10,21 @@
 ;;       Visualization &
 ;;         Tangibility
 ;; ===========================
-(defn- border? [height width x y]
-  (or (= x (quot width 2)) (= y (quot height 2)) ))
-
-(defn- axis? [height width x y]
+(defn- border? [{:as spec :keys [width height]} x y]
   (or (#{0 (dec width)} x) (#{0 (dec height)} y)))
 
-(defn axes-grid [{:keys [width height unit]}]
+(defn- axis? [{:as spec :keys [width height]} x y]
+  (let [[x-offset y-offset] (origin-offset spec)]
+    (or (= x x-offset) (= y y-offset))))
+
+(defn axes-grid [{:as spec :keys [width height unit]}]
   (let [img (BufferedImage. width height BufferedImage/TYPE_INT_RGB)]
     (dotimes [x width]
       (dotimes [y height]
         (.setRGB img x y
                  (cond
-                   (axis? height width x y) 0x00
-                   (border? height width x y) 0xFF
+                   (axis? spec x y) 0xFF
+                   (border? spec x y) 0x00
                    :else 0xF0F0F0 #_(+ 0xFFFFFF (* 10 x) (* 10 y))))))
     img))
 
@@ -34,26 +35,70 @@
              (>= y 0) (< y (.getHeight img)))
     (.setRGB img x y rgb)))
 
+(defn project [{:as spec
+                :keys [width height]
+                [x-range y-range] :range}]
+  (let [[x-unit y-unit] (unit-lengths spec)
+        [x-offset y-offset] (origin-offset spec)]
+    (fn [[x y]]
+      [(+ x-offset (* x x-unit)) (- y-offset (* y y-unit))])))
+
+(defn fill-out [spec]
+  (merge {:range [[-3 3] [-3 3]]
+          :width 300
+          :height 300}
+         spec))
+
+;; Glossary
+;; ---------
+;; Offset   The coordinate of a pixel in a raster
+
+(defn unit-lengths [{:as spec
+                     :keys [width height]
+                     [[x-min x-max] [y-min y-max]] :range}]
+  (let [x-units (- x-max x-min)
+        y-units (- y-max y-min)]
+    [(/ width x-units) (/ height y-units)]))
+
+(defn origin-offset [{:as spec
+                      :keys [height]
+                      [[x-min x-max] [y-min y-max]] :range}]
+  (let [[x-unit y-unit] (unit-lengths spec)]
+    [(* x-unit (- x-min)) (+ height (* y-unit y-min))]))
+
+(defn draw-dot [img x y]
+  (set-rgb img x (dec y) 0)
+  (set-rgb img (dec x) y 0)
+  (set-rgb img x y 0)
+  (set-rgb img (inc x) y 0)
+  (set-rgb img x (inc y) 0))
+
 (defn plot-points [{:as spec :keys [range points]}]
   ;; WT=wishful thought
   ;; WT: axes-grid understand range
-  (let [grid (axes-grid spec)]
-    (doseq [[x y] (map (project spec) points)]
-      (draw-dot img x y))
+  (let [spec (fill-out spec)
+        grid (axes-grid spec)]
+    (transduce (map (project spec))
+               (completing (fn [_ [x y]] (draw-dot grid x y)))
+               nil
+               points)
     grid))
 
-(defn plot [x]
-  (cond
-    (fn? x) (plot {:f x
-                   :width 600
-                   :height 300})
-    (:points x) (plot-points x)
-    :else (let [{:keys [f height width]} x
-                grid (axes-grid x)
-                origin-y (quot height 2)
-                origin-x (quot width 2)]
-            (doseq [x (range (- (quot width 2)) (quot width 2))]
-              #_(set-rgb grid x (f x) 0xFF00)
-              (set-rgb grid (+ origin-x x) (- origin-y (f x))  0xFF00))
-            grid)))
+(defn plot-fn [{:as spec
+                :keys [f height width]
+                [[x-min x-max]] :range}]
+  (let [grid (axes-grid spec)]
+    (doseq [[x y] (->> (range x-min x-max 0.001)
+                       (map (juxt identity f))
+                       (map (project spec)))]
+      (set-rgb grid x y  0xFF00))
+    grid))
+
+(defn plot [spec]
+  (if (fn? spec)
+    (plot {:f spec})
+    (let [spec (fill-out spec)]
+      (if (:points spec)
+        (plot-points spec)
+        (plot-fn spec)))))
 
